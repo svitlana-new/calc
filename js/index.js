a714381cebcd545c79f86e1a0ec90ade4028cad9
svitlana-new <galka.mospan@gmail.com>
2020-05-09 16:49:27 +0000

    added memory buttons

--- js/index.js
+++ js/index.js
@@ -1,38 +1,53 @@
-//const btnMemo = document.querySelectorAll('.memo button');
-//btnMemo.forEach(memo => memo.addEventListener('click', memoPressed));
+const btnMemoAdd = document.querySelector('.memoAdd');
+btnMemoAdd.addEventListener('click', memoAddPressed);
 
 const display = document.querySelector('.display');
+let memory = 0;
 
-//function memoPressed (ev) {
- //   display.value += ev.target.innerText;
-//}
+function memoAddPressed() {
+    memory += display.value;
+}
+
+const btnMemoSub = document.querySelector('.memoSub');
+btnMemoSub.addEventListener('click', memoSubPressed);
+
+function memoSubPressed() {
+    memory -= display.value;
+}
+
+const btnMemoRet = document.querySelector('.return-memo');
+btnMemoRet.addEventListener('click', memoRetPressed);
+
+function memoRetPressed() {
+    display.value = memory;
+}
 
 const btnOpers = document.querySelectorAll('.opers button');
 btnOpers.forEach(opers => opers.addEventListener('click', opersPressed));
 
-function opersPressed (ev) {
+function opersPressed(ev) {
     display.value += ev.target.innerText;
-      
-    }
+
+}
 
 const btnDigits = document.querySelectorAll('.digits button');
 btnDigits.forEach(digit => digit.addEventListener('click', digitPressed));
 
-function digitPressed (ev) {
+function digitPressed(ev) {
     display.value += ev.target.innerText;
 }
 
 const btnEqual = document.querySelector('.equal');
 btnEqual.addEventListener('click', equalPressed);
 
-function equalPressed () {
+function equalPressed() {
     display.value = (eval(display.value));
 }
 
 const btnClean = document.querySelector('.clean');
 btnClean.addEventListener('click', clickPressed);
 
-function clickPressed () {
+function clickPressed() {
     display.value = null;
 }
 
@@ -40,11 +55,10 @@ function clickPressed () {
 const btnZero = document.querySelector('.zero');
 btnZero.addEventListener('click', divByZero)
 
-function divByZero (display) {
+function divByZero(display) {
 
-if (display.value === '/0')
-{
-    btnEqual.preventDefault()
-    alert('Ділити на нуль не можна:)')
-}
+    if (display.value === '/0') {
+        btnEqual.preventDefault()
+        alert('Ділити на нуль не можна')
+    }
 }
